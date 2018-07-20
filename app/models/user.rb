8e655d721d9888e3b733f58acbc2ac9df5a8b140
scratchpadhell <gypsyoak@yahoo.com>
2018-07-20 16:05:59 -0500

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
   include HTTParty
 
-
-  validates :name, presence: true
   validates :email, presence: true
 
   def get_iss
